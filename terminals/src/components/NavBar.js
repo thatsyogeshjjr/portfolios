eba2e238b7c07fd42bf7c989b8772e50d9d97fef
Yogesh Jajoria <yogeshjajoria2019@gmail.com>
2024-04-21 19:46:35 +0530

    header ready

--- terminals/src/components/NavBar.js
+++ terminals/src/components/NavBar.js
@@ -3,17 +3,19 @@ import "../css/NavBar.css";
 export var NavBar = () => {
   return (
     <div id="header">
-      <div className="title">
-        <h1>PORTFOLIO</h1>
-      </div>
-      <div className="nav">
-        <ul id="nav-list">
-          <li class="nav-item">Home</li>
-          <li class="nav-item">My Projects</li>
-          <li class="nav-item">Resume</li>
-          <li class="nav-item">Publications</li>
-          <li class="nav-item">Contact Me</li>
-        </ul>
+      <div className="container">
+        <div className="title">
+          <h1>PORTFOLIO</h1>
+        </div>
+        <div className="nav">
+          <ul id="nav-list">
+            <li class="nav-item">Home</li>
+            <li class="nav-item">My Projects</li>
+            <li class="nav-item">Resume</li>
+            <li class="nav-item">Publications</li>
+            <li class="nav-item">Contact Me</li>
+          </ul>
+        </div>
       </div>
     </div>
   );

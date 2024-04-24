328b897b19cd5fc96a06dd10330bfbe768b39d77
Yogesh Jajoria <yogeshjajoria2019@gmail.com>
2024-04-24 23:40:10 +0530

    fixed focus on terminal input

--- terminals/src/components/Terminal.js
+++ terminals/src/components/Terminal.js
@@ -1,23 +1,31 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import "../css/Terminal.css";
 
 export var Terminal = (props) => {
   var terminal_id = props.id;
-  var text,
-    setText = useState("hello world");
+  var default_dev_info = ">_ ";
+  var [text, setText] = useState(default_dev_info);
+  var terminal_class = `terminal ${props.type}`;
+  var inputRef = useRef("");
+  function focusInput() {
+    inputRef.current.focus();
+  }
 
   return (
-    <div className="terminal">
+    <div className={terminal_class} onClick={focusInput}>
       <div className="titlebar">
         <div id="title-content">{terminal_id}</div>
         <div className="title-buttons">
-          <button className="t-btn green"></button>
-          <button className="t-btn orange"></button>
-          <button className="t-btn red"></button>
+          <button className="t-btn"></button>
+          <button className="t-btn"></button>
+          <button className="t-btn"></button>
         </div>
       </div>
       <hr id="gap" />
-      <div className="data">{text}</div>
+      <div className="data">
+        {text}
+        <input type="text" ref={inputRef}></input>
+      </div>
     </div>
   );
 };

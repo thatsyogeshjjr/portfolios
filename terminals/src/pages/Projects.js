49c4d2284298c3e290804c68ff5cc3d8c7bc530d
Yogesh Jajoria <yogeshjajoria2019@gmail.com>
2024-04-25 22:16:50 +0530

    data can now be used to create elements

--- terminals/src/pages/Projects.js
+++ terminals/src/pages/Projects.js
@@ -1,23 +1,34 @@
 import { initializeApp } from "firebase/app";
 import { getDatabase, ref, onValue, get } from "firebase/database";
 import { NavBar } from "../components/NavBar.js";
+import { useEffect, useState } from "react";
 
 export var ProjectPage = () => {
-  const projectsInDB = setupDatabase();
+  const [projData, setProjData] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      var data;
+      const appSettings = {
+        databaseURL: process.env.REACT_APP_databaseURL,
+      };
+
+      const app = initializeApp(appSettings);
+      const projects = ref(getDatabase(app), "projects");
+      onValue(projects, (snapshot) => {
+        data = snapshot.val();
+      });
+      setProjData(data);
+    }
+    fetchData();
+  }, []);
   return (
     <>
       <NavBar />
+      {projData.map((item) => {
+        return <h2>{item.proj_name}</h2>;
+      })}
     </>
   );
 };
 
-function setupDatabase() {
-  const appSettings = {
-    databaseURL: process.env.REACT_APP_databaseURL,
-  };
-
-  const app = initializeApp(appSettings);
-  const projects = ref(getDatabase(app), "projects");
-
-  return projects;
-}
+function getData() {}

6b9bc63f0f1e39f636b6a902652dccf010e3b199
Yogesh Jajoria <yogeshjajoria2019@gmail.com>
2024-04-25 22:23:24 +0530

    deleted old files, pages folder added with files.

--- terminals/src/App.js
+++ terminals/src/App.js
@@ -1,12 +1,17 @@
 import "./css/App.css";
-import { NavBar } from "./components/NavBar.js";
-import { Terminal } from "./components/Terminal.js";
+import { ContactPage } from "./pages/Contact.js";
+import { HomePage } from "./pages/Home.js";
+import { ProjectPage } from "./pages/Projects.js";
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
-      <Terminal id="Terminal 1" />
+      {/* <NavBar /> */}
+      {/* <Terminal id="Terminal 1" type="main" />
+      <Terminal id="Terminal 1" type="bg" /> */}
+      {/* <ContactPage /> */}
+      {/* <HomePage /> */}
+      <ProjectPage />
     </div>
   );
 }

7fa377b704931a38424d5cde86ca9205d2d2361d
Yogesh Jajoria <yogeshjajoria2019@gmail.com>
2024-04-28 21:05:47 +0530

    publications page made

--- terminals/src/pages/Publications.js
+++ terminals/src/pages/Publications.js
@@ -1,1 +1,45 @@
-export var PublicationsPage = () => {};
+import { db } from "../config/firebase.js";
+import { getDocs, collection } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import "../css/Projects.css";
+
+export var PublicationsPage = () => {
+  const [pubData, setPubData] = useState([]);
+  const pubCollectionRef = collection(db, "publications");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await getDocs(pubCollectionRef);
+        const filteredData = data.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc.id,
+        }));
+        setPubData(filteredData);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      <div className="content-project">
+        <div className="card-list right">
+          {pubData.map((pub) => (
+            <a href={pub.link} target="_blank" rel="noreferrer">
+              <div className="card" key={pub.id}>
+                <div className="empty-space"></div>
+                <div className="card-data">
+                  <h3 id="proj-name">{pub.name}</h3>
+                  <p id="proj-desc">{pub.desc}</p>
+                </div>
+              </div>
+            </a>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};

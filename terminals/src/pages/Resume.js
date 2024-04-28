aec826056fc8f3e234556a86bae8668de9f41a45
Yogesh Jajoria <yogeshjajoria2019@gmail.com>
2024-04-28 18:50:10 +0530

    coming soon added to resume

--- terminals/src/pages/Resume.js
+++ terminals/src/pages/Resume.js
@@ -1,1 +1,14 @@
-export var ResumePage = () => {};
+import { Link } from "react-router-dom";
+import "../css/Resume.css";
+
+export var ResumePage = () => {
+  return (
+    <div className="resume-content">
+      <h1>coming soon... </h1>
+      <h4>
+        Check out my <Link to="../projects">projects</Link> or{" "}
+        <Link to="../contact">LinkedIn</Link> in that time...
+      </h4>
+    </div>
+  );
+};

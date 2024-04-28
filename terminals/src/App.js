7f6e4a58bd8f72be7a34a686a34db80fb3c5fb8e
Yogesh Jajoria <yogeshjajoria2019@gmail.com>
2024-04-28 18:26:55 +0530

    handling page routing in App.js

--- terminals/src/App.js
+++ terminals/src/App.js
@@ -1,17 +1,26 @@
 import "./css/App.css";
+import { NavBar } from "./components/NavBar.js";
 import { ContactPage } from "./pages/Contact.js";
 import { HomePage } from "./pages/Home.js";
 import { ProjectPage } from "./pages/Projects.js";
-
+import { PublicationsPage } from "./pages/Publications.js";
+import { ResumePage } from "./pages/Resume.js";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      {/* <NavBar /> */}
-      {/* <Terminal id="Terminal 1" type="main" />
-      <Terminal id="Terminal 1" type="bg" /> */}
-      {/* <ContactPage /> */}
-      {/* <HomePage /> */}
-      <ProjectPage />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<NavBar />}>
+            <Route index element={<HomePage />} />
+            <Route path="projects" element={<ProjectPage />} />
+            <Route path="resume" element={<ResumePage />} />
+            <Route path="publications" element={<PublicationsPage />} />
+            <Route path="contact" element={<ContactPage />} />
+            {/* <Route path="*" element={<NoPage />} /> */}
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }

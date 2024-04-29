cb258391014d19db6e2fe69f5fbe67c612fbe158
Yogesh Jajoria <yogeshjajoria2019@gmail.com>
2024-04-29 12:47:03 +0530

    changes to work with taskbar and some design modifications

--- terminals/src/components/Terminal.js
+++ terminals/src/components/Terminal.js
@@ -1,31 +1,68 @@
 import { useRef, useState } from "react";
 import "../css/Terminal.css";
+import Draggable from "react-draggable";
 
 export var Terminal = (props) => {
-  var terminal_id = props.id;
-  var default_dev_info = ">_ ";
-  var [text, setText] = useState(default_dev_info);
-  var terminal_class = `terminal ${props.type}`;
+  const terminal_id = props.id;
+  const welcomeText = "sample-welcome-text";
+  var terminalRef = useRef();
+  const terminal_class = `terminal ${props.type}`;
+  const commands = new Map([
+    [
+      "info yogesh.jjr",
+      "  <h2>Yogesh Jajoria</h2><h4>Tech Fanatic</h4><h4>Programmer (JS, React, Flutter, Python, ... )</h4><h4>Violin Student</h4>",
+    ],
+  ]);
   var inputRef = useRef("");
+  var resultRef = useRef();
   function focusInput() {
     inputRef.current.focus();
   }
 
+  function displayResult(e) {
+    resultRef.current.innerHTML =
+      commands.get(e.currentTarget.value) === undefined
+        ? "Command not found"
+        : commands.get(e.currentTarget.value);
+  }
+
+  function removeApp() {
+    terminalRef.current.style.display = "none";
+  }
+
   return (
-    <div className={terminal_class} onClick={focusInput}>
-      <div className="titlebar">
-        <div id="title-content">{terminal_id}</div>
-        <div className="title-buttons">
-          <button className="t-btn"></button>
-          <button className="t-btn"></button>
-          <button className="t-btn"></button>
+    <Draggable>
+      <div
+        id="terminal"
+        className={terminal_class}
+        onClick={focusInput}
+        ref={terminalRef}
+      >
+        <div className="titlebar">
+          <div id="title-content">{terminal_id}</div>
+          <div className="title-buttons">
+            <button className="t-btn" onClick={removeApp}></button>
+          </div>
+        </div>
+        <hr id="gap" />
+        <div className="data">
+          {props.type === "main" ? <></> : welcomeText}
+          <input
+            type="text"
+            ref={inputRef}
+            defaultValue="info yogesh.jjr"
+            on
+            onChange={(e) => displayResult(e)}
+            onEnter
+          ></input>
+          <p id="result" ref={resultRef}>
+            <h2>Yogesh Jajoria</h2>
+            <h4>Tech Fanatic</h4>
+            <h4>Programmer (JS, React, Flutter, Python, ... )</h4>
+            <h4>Violin Student</h4>
+          </p>
         </div>
       </div>
-      <hr id="gap" />
-      <div className="data">
-        {text}
-        <input type="text" ref={inputRef}></input>
-      </div>
-    </div>
+    </Draggable>
   );
 };

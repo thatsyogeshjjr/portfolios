4c640ea8a13b436f9d0e6ce59802db617b66b6d6
Yogesh Jajoria <yogeshjajoria2019@gmail.com>
2024-05-27 23:54:44 +0530

    basic structure

--- mobile_first/src/App.jsx
+++ mobile_first/src/App.jsx
@@ -1,10 +1,15 @@
 import "./App.css";
+import backgroundImg from "./assets/bg.png";
 
 function App() {
   return (
     <>
-      <div>
-        <h1>hello world!</h1>
+      <div className="flex-col justify-center items-center">
+        <h1 className="font-bold">Portfolio</h1>
+        <img src={backgroundImg} alt="" />
+        <h2 className="font-xl">Yogesh Jajoria</h2>
+        <p>Your friendly neighbourhood programmer.</p>
+        <button>My Projects</button>
       </div>
     </>
   );

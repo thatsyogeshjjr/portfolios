baef12704bc0cadffcd4c467e21defb1d3f399b1
Yogesh Jajoria <yogeshjajoria2019@gmail.com>
2024-05-28 00:27:05 +0530

    UI update, deleted unused file
    Changes to be committed:
    	deleted:    mobile_first/public/vite.svg
    	modified:   mobile_first/src/App.jsx

--- mobile_first/src/App.jsx
+++ mobile_first/src/App.jsx
@@ -4,12 +4,27 @@ import backgroundImg from "./assets/bg.png";
 function App() {
   return (
     <>
-      <div className="flex-col justify-center items-center">
-        <h1 className="font-bold">Portfolio</h1>
-        <img src={backgroundImg} alt="" />
-        <h2 className="font-xl">Yogesh Jajoria</h2>
-        <p>Your friendly neighbourhood programmer.</p>
-        <button>My Projects</button>
+      <div className="flex flex-col align-middle justify-between h-screen">
+        <h1 className="font-bold text-xl p-5">Portfolio</h1>
+        <div
+          id="content"
+          className="flex flex-col items-center text-start *:m-2 w-screen"
+        >
+          <img src={backgroundImg} alt="" className="w-min justify-center" />
+          <div className="*:m-6">
+            <h2 className="font-semibold text-5xl ">Yogesh Jajoria</h2>
+            <p className="text-md">
+              <span>Your friendly neighbourhood</span>
+              <br />
+              <span>programmer_</span>
+            </p>
+            <button className="bg-slate-200 font-semibold rounded-md p-1 text-slate-950 w-40">
+              My Projects
+            </button>
+          </div>
+        </div>
+
+        <div id="empty"></div>
       </div>
     </>
   );
